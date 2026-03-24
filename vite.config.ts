7874c1e02e9fe57b35c9294387374e18ae52010f
matej <matejhanzlik25@gmail.com>
2026-03-24 10:28:57 +0100

    .githubl

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-      tailwindcss()
-  ],
+    base: "/passwordGame/",
+
+    plugins: [react()],
+    server: {
+        host: true,
+        port: 5173,
+        strictPort: true
+    },
+    optimizeDeps: {
+        force: true
+    }
 })
